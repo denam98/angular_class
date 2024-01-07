8afbc18bd322300b05baeab54e81493f6a48f837
denam98 <dchamath5@gmail.com>
2024-01-07 21:29:11 +0530

    day 07

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { AuthService } from '../../../shared/services/auth.service';
+import { User } from '../../../shared/interfaces/User';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +14,7 @@ export class LoginComponent {
   email: string = '';
   password: string = '';
 
-  constructor(){
+  constructor(private authService: AuthService){
 
   }
 
@@ -20,5 +22,24 @@ export class LoginComponent {
     this.submitted = true;
     // alert('email => ' + this.email + "\npassword => " + this.password);
     console.log('email => ' + this.email + "\npassword => " + this.password);
+
+    this.authService.authenticate(this.email, this.password)
+    .then((result:User[]) => {
+      console.log(result);
+      const user = result.find((item: User) => item.username === this.email);
+      if (user != undefined) {
+        if(user.password === this.password){
+          alert('login success');
+          console.log(user);
+        } else {
+          alert('Invalid password');
+        }
+      } else {
+        alert('No such user found');
+      }
+    }).catch(error => {
+      console.error(error);
+    });
   }
+
 }
---
+++ src/shared/services/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { User } from '../interfaces/User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl: string = 'http://localhost:3000'
+
+  constructor() { }
+
+  async authenticate(email: string, password: string): Promise<User[]>{
+    const data = await fetch(`${this.baseUrl}/users`);
+    return await data.json() ?? [] ;
+  }
+}
